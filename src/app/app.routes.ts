0019697068083c3cdeb4b35afc9ff0e4f55a94de
Petar Kresoja <pkresoja@singidunum.ac.rs>
2025-09-23 10:47:33 +0200

    Kraj prvog dana
    Dodali smo bootstrap 5.3 i navigaciju u korenu komponentu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Details } from './details/details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'login', component: Login },
+    { path: 'signup', component: Signup },
+    { path: 'details/:id', component: Details }
+]
