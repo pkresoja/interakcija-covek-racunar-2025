ca53edc532e1f03e86634a403ec9b93f96beb0fb
Petar Kresoja <pkresoja@singidunum.ac.rs>
2025-09-30 09:44:56 +0200

    Fixed navbar and added user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,39 @@
+import { UserModel } from "../models/user.model";
+
+export class UserService {
+    static getUsers(): UserModel[] {
+        if (!localStorage.getItem('icr_users')) {
+            localStorage.setItem('icr_users', JSON.stringify([
+                {
+                    firstName: "Example",
+                    lastName: "User",
+                    email: "user@example.com",
+                    phone: "+38169321321",
+                    password: "user123",
+                    data: []
+                }
+            ]))
+        }
+
+        return JSON.parse(localStorage.getItem('icr_users')!)
+    }
+
+    static findUserByEmail(email: string) {
+        const users = this.getUsers()
+        const selectedUser = users.find(u => u.email === email)
+
+        if (!selectedUser)
+            throw new Error('USER_NOT_FOUND')
+
+        return selectedUser
+    }
+
+    static login(email: string, password: string) {
+        try {
+            const user = this.findUserByEmail(email)
+            return user.password === password
+        } catch {
+            return false
+        }
+    }
+}
